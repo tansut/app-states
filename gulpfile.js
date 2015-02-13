f6d9819d184988e0d94b8a2286a1ccd009f645d8
kalitte <tansut@gmail.com>
2015-02-13 15:22:50 +0200

    cleared gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,19 @@
-'use strict';
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var inline = require('gulp-inline');
-var karma = require('gulp-karma');
 var vulcanize = require('gulp-vulcanize');
+var gulpFilter = require('gulp-filter');
 
-var files = ['src/*.js', 'src/*.html', 'tests/spec/*.js'];
-
-gulp.task('merge', function () {
-    return gulp.src('src/app-states.html')
-        .pipe(vulcanize({
-            dest: 'src/app-states-merged.html',
-            strip: true
-        }))
-        .pipe(gulp.dest('.'));
-});
-
-gulp.task('lint', function() {
-  return gulp.src(files)
-    .pipe(jshint.extract('always'))
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('build', function() {
-  return gulp.src('src/app-states-merged.html')
-    .pipe(uglify({
-      preserveComments: 'some'
+gulp.task('merge', function() {
+  return gulp.src('src/app-states.html')
+    .pipe(vulcanize({
+      dest: './src',
+      strip: true,
+      csp: true
     }))
-    .pipe(gulp.dest('app-states.html'));
+    .pipe(gulp.dest('.'))
+    .pipe(gulpFilter(['*.js']))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'));
 });
 
-gulp.task('minify', function() {
-  return gulp.src('src/app-states-merged.html')
-    .pipe(inline({
-      base: 'src/',
-      js: uglify()
-    }))
-    .pipe(gulp.dest('app-states.html'));
-});
-
-
-// watch
-gulp.task('watch', function() {
-  gulp.watch(files, ['lint', 'merge', 'build', 'minify'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-});
-
-// default
-gulp.task('default', ['lint', 'merge', 'build', 'minify']);
-
-// Travis CI
-gulp.task('ci', ['lint']);
+gulp.task('default', ['merge']);
